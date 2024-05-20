bb0dff5644136af74829ef5a02b25dd03488a532
Tomazauras <97549370+Tomazauras@users.noreply.github.com>
2024-05-20 04:08:12 +0300

    UserID added to uploadPost

--- client/src/views/Posts management subsystem/Upload.js
+++ client/src/views/Posts management subsystem/Upload.js
@@ -1,200 +1,227 @@
 import React, { Component } from "react";
-import "../../styles.css"; 
+import "../../styles.css";
 import axios from "axios";
+import firebase from "firebase/compat/app";
 
 class Upload extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            allData: [],
-            title: "",
-            price: "",
-            description: "",
-            image: null,
-            uploading: false,
-            categoryId: null,
-            deepestCategories: []
-        };
-    }
-
-    componentDidMount() {
-        // Fetch existing categories when the component mounts
-        this.showCategoryList();
-    }
-
-    startUpload() {
-        const { allData, title, price, description, image, uploading, categoryId } = this.state;
-
-        const formData = new FormData();
-        formData.append("title", title);
-        formData.append("price", price);
-        formData.append("description", description);
-        formData.append("categoryId", categoryId); 
-        formData.append("image", image); 
-        this.setState({ uploading: true });
-
-        axios.post('http://localhost:5000/upload', formData)
-        .then(response => {
-            // Redirect to the login page or handle success response
-            if (response.data && response.data.redirectTo) {
-                window.location.href = response.data.redirectTo; // Redirect to login page
-                window.alert("Upload successful!");
-            } 
-            else if (response.data.message == 1) {
-                this.setState({ message: "**Title is empty" });
-            }
-            else if (response.data.message == 2) {
-                this.setState({ message: "**Description is empty" });
-            }
-            else if (response.data.message == 3) {
-                this.setState({ message: "**Incorrect price" });
-            }
-            else if (response.data.message == 4) {
-                this.setState({ message: "**No image selected or it's size is incorrect." });
-            } else if (response.data.message == 5) {
-                this.setState({ message: "**No category selected" });
-            }
-            
-          })
-          .catch(error => {
-              
-            console.error("Error:", error);
-          }).finally(() => {
-
-            this.setState({ uploading: false });
-        });
-    }
-
-    handleTitleChange = (e) => {
-        this.setState({ title: e.target.value });
+  constructor(props) {
+    super(props);
+    this.state = {
+      allData: [],
+      title: "",
+      price: "",
+      description: "",
+      image: null,
+      uploading: false,
+      categoryId: null,
+      deepestCategories: [],
+      userID: null,
     };
+  }
 
-    handleDescriptionChange = (e) => {
-        this.setState({ description: e.target.value });
-    };
+  componentDidMount() {
+    // Fetch existing categories when the component mounts
+    this.showCategoryList();
 
-    handlePriceChange = (e) => {
-        this.setState({ price: e.target.value });
-    };
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        const userID = firebase.auth().currentUser.uid;
+        this.setState({ userID });
+      }
+    });
+  }
 
-    handleUpload = () => {
-        this.startUpload();
-    };
+  startUpload() {
+    const {
+      allData,
+      title,
+      price,
+      description,
+      image,
+      uploading,
+      categoryId,
+      userID,
+    } = this.state;
+    const formData = new FormData();
+    formData.append("title", title);
+    formData.append("price", price);
+    formData.append("description", description);
+    formData.append("categoryId", categoryId);
+    formData.append("image", image);
+    formData.append("fk_userID", userID);
+    this.setState({ uploading: true });
 
-    handleImageChange = (e) => {
-        this.setState({ image: e.target.files[0] }); // Get the first file from the selected files array
-        
-    };
+    axios
+      .post("http://localhost:5000/upload", formData)
+      .then((response) => {
+        // Redirect to the login page or handle success response
+        if (response.data && response.data.redirectTo) {
+          window.location.href = response.data.redirectTo; // Redirect to login page
+          window.alert("Upload successful!");
+        } else if (response.data.message == 1) {
+          this.setState({ message: "**Title is empty" });
+        } else if (response.data.message == 2) {
+          this.setState({ message: "**Description is empty" });
+        } else if (response.data.message == 3) {
+          this.setState({ message: "**Incorrect price" });
+        } else if (response.data.message == 4) {
+          this.setState({
+            message: "**No image selected or it's size is incorrect.",
+          });
+        } else if (response.data.message == 5) {
+          this.setState({ message: "**No category selected" });
+        }
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      })
+      .finally(() => {
+        this.setState({ uploading: false });
+      });
+  }
 
-    showCategoryList() {
-        axios.post("http://localhost:5000/fetchCategorys")
-            .then(response => {
-                const allData = response.data.AllData;
-                const deepestCategories = this.getDeepestCategories(allData);
-                this.setState({ allData, deepestCategories });
-            })
-            .catch(error => {
-                console.error("Error:", error);
-            });
-    }
+  handleTitleChange = (e) => {
+    this.setState({ title: e.target.value });
+  };
 
-    renderCategoryHierarchy(categoryId, allData) {
-        const category = allData.find(cat => cat.id === categoryId);
-        if (!category) return null;
+  handleDescriptionChange = (e) => {
+    this.setState({ description: e.target.value });
+  };
 
-        const parentHierarchy = this.renderCategoryHierarchy(category.tevoId, allData);
+  handlePriceChange = (e) => {
+    this.setState({ price: e.target.value });
+  };
 
-        return (
-            <div key={category.id}>
-                {parentHierarchy}
-                {parentHierarchy && " > "}
-                {category.pavadinimas}
-            </div>
-        );
-    }
+  handleUpload = () => {
+    this.startUpload();
+  };
 
-    getDeepestCategories(categories) {
-        const parentIds = new Set(categories.map(category => category.tevoId));
-        return categories.filter(category => !parentIds.has(category.id));
-    }
+  handleImageChange = (e) => {
+    this.setState({ image: e.target.files[0] }); // Get the first file from the selected files array
+  };
 
-    handleCategorySelect = event => {
-        const categoryId = event.target.value === "" ? null : event.target.value;
-        
-        this.setState({ categoryId });
-    };
+  showCategoryList() {
+    axios
+      .post("http://localhost:5000/fetchCategorys")
+      .then((response) => {
+        const allData = response.data.AllData;
+        const deepestCategories = this.getDeepestCategories(allData);
+        this.setState({ allData, deepestCategories });
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }
 
-    render() {
-        const {
-            allData,
-            title,
-            price,
-            description,
-            message,
-            uploading,
-            categoryId,
-            deepestCategories
-          } = this.state;
+  renderCategoryHierarchy(categoryId, allData) {
+    const category = allData.find((cat) => cat.id === categoryId);
+    if (!category) return null;
 
-        return (
-            <div className="container">
-                <div className="header">Skelbimo įkėlimas</div>
-                {message && <div className="error-message"><b>{message}</b></div>}
-                <div>
-                    <input
-                        type="text"
-                        name="title"
-                        placeholder="Pavadinimas"
-                        value={title}
-                        onChange={this.handleTitleChange}
-                        className="input-field"
-                    />
-                </div>
+    const parentHierarchy = this.renderCategoryHierarchy(
+      category.tevoId,
+      allData
+    );
 
-                <div>
-                    <textarea
-                        type="text"
-                        name="description"
-                        placeholder="Aprašymas"
-                        value={description}
-                        onChange={this.handleDescriptionChange}
-                        className="input-field"
-                    />
-                </div>
+    return (
+      <div key={category.id}>
+        {parentHierarchy}
+        {parentHierarchy && " > "}
+        {category.pavadinimas}
+      </div>
+    );
+  }
 
-                <div>
-                    <input
-                        type="number"
-                        name="price"
-                        placeholder="Kaina"
-                        value={price}
-                        onChange={this.handlePriceChange}
-                        className="input-field"
-                    />
-                </div>
-                <label>Kategorija:</label>
-                <select onChange={this.handleCategorySelect}>
-                    <option value="">-</option>
-                    {deepestCategories.map(category => (
-                        <option key={category.id} value={category.id}>
-                            {this.renderCategoryHierarchy(category.id, allData)}
-                        </option>
-                    ))}
-                </select>
-                <div>
-                    <input
-                        type="file"
-                        accept="image/*" // Accept only image files
-                        onChange={this.handleImageChange}
-                        className="input-field"
-                    />
-                </div>
-                {uploading && <div><b>Įkeliama...</b></div>}
-                <button className="uploadClick" onClick={this.handleUpload}>Išsaugoti</button>
+  getDeepestCategories(categories) {
+    const parentIds = new Set(categories.map((category) => category.tevoId));
+    return categories.filter((category) => !parentIds.has(category.id));
+  }
 
-            </div>
-        );
-    }
+  handleCategorySelect = (event) => {
+    const categoryId = event.target.value === "" ? null : event.target.value;
+
+    this.setState({ categoryId });
+  };
+
+  render() {
+    const {
+      allData,
+      title,
+      price,
+      description,
+      message,
+      uploading,
+      categoryId,
+      deepestCategories,
+    } = this.state;
+
+    return (
+      <div className="container">
+        <div className="header">Skelbimo įkėlimas</div>
+        {message && (
+          <div className="error-message">
+            <b>{message}</b>
+          </div>
+        )}
+        <div>
+          <input
+            type="text"
+            name="title"
+            placeholder="Pavadinimas"
+            value={title}
+            onChange={this.handleTitleChange}
+            className="input-field"
+          />
+        </div>
+
+        <div>
+          <textarea
+            type="text"
+            name="description"
+            placeholder="Aprašymas"
+            value={description}
+            onChange={this.handleDescriptionChange}
+            className="input-field"
+          />
+        </div>
+
+        <div>
+          <input
+            type="number"
+            name="price"
+            placeholder="Kaina"
+            value={price}
+            onChange={this.handlePriceChange}
+            className="input-field"
+          />
+        </div>
+        <label>Kategorija:</label>
+        <select onChange={this.handleCategorySelect}>
+          <option value="">-</option>
+          {deepestCategories.map((category) => (
+            <option key={category.id} value={category.id}>
+              {this.renderCategoryHierarchy(category.id, allData)}
+            </option>
+          ))}
+        </select>
+        <div>
+          <input
+            type="file"
+            accept="image/*" // Accept only image files
+            onChange={this.handleImageChange}
+            className="input-field"
+          />
+        </div>
+        {uploading && (
+          <div>
+            <b>Įkeliama...</b>
+          </div>
+        )}
+        <button className="uploadClick" onClick={this.handleUpload}>
+          Išsaugoti
+        </button>
+      </div>
+    );
+  }
 }
 
 export default Upload;
--- server/models/Post.js
+++ server/models/Post.js
@@ -1,128 +1,164 @@
 const firebaseAdmin = require("../config/firebase-config");
 
 class Post {
-    static async addPost(userData) {
-        try {
-            const { title,
-                description,
-                price,
-                state,
-                urlId,
-                categoryId
-                } = userData;
-                const postId = firebaseAdmin.firestore().collection("posts").doc().id;
+  static async addPost(userData) {
+    try {
+      const { title, description, price, state, urlId, categoryId, fk_userID } =
+        userData;
+      const postId = firebaseAdmin.firestore().collection("posts").doc().id;
 
-                const postRef = await firebaseAdmin.firestore().collection("posts").doc(postId).set({
-                    title,
-                    description,
-                    price,
-                    state,
-                    urlId,
-                    categoryId
-                });
-                
-                return postId;
-        } catch (error) {
-            throw error;
-        }
-    }
-
-    static async addImageToStorage(image){
-        const bucket = firebaseAdmin.storage().bucket();
-        const fileName = `${image.originalname}`;
-        const file = bucket.file(fileName);
-        await file.save(image.buffer, {
-            metadata: {
-                contentType: image.mimetype
-            }
+      const postRef = await firebaseAdmin
+        .firestore()
+        .collection("posts")
+        .doc(postId)
+        .set({
+          title,
+          description,
+          price,
+          state,
+          urlId,
+          categoryId,
+          fk_userID,
         });
 
-        return fileName;
+      return postId;
+    } catch (error) {
+      throw error;
     }
+  }
 
-    static async addImageToDatabase(url){
-        const posts_images_id = firebaseAdmin.firestore().collection("posts_images").doc().id;
-        const postRef = await firebaseAdmin.firestore().collection("posts_images").doc(posts_images_id).set({
-            url
-        });
-        return posts_images_id;
-    }
+  static async addImageToStorage(image) {
+    const bucket = firebaseAdmin.storage().bucket();
+    const fileName = `${image.originalname}`;
+    const file = bucket.file(fileName);
+    await file.save(image.buffer, {
+      metadata: {
+        contentType: image.mimetype,
+      },
+    });
 
-    static async getAllPosts() {
-        try {
-            const snapshot = await firebaseAdmin.firestore().collection("posts").get();
-            const posts = await Promise.all(snapshot.docs.map(async doc => {
-                const postData = doc.data();
-                const docSnapshot = await firebaseAdmin.firestore().collection("posts_images").doc(postData.urlId).get(); 
-                const url = docSnapshot.data().url; // Assuming 'url' is the field containing the storage file URL
-                const imageUrl = await firebaseAdmin.storage().bucket().file(url).getSignedUrl({ action: 'read', expires: '01-01-2025' });
-                return { id: doc.id, ...postData, imageUrl };
-            }));
-            return posts;
-        } catch (error) {
-            console.error("Error fetching all posts:", error);
-            throw error; // Rethrow the error to the caller for proper error handling
-        }
-    }
+    return fileName;
+  }
 
-    static async getPostById(postId) {
-        try {
-            const docSnapshot = await firebaseAdmin.firestore().collection("posts").doc(postId).get();
-            
-            if (!docSnapshot.exists) {
-                return null; // Return null if the post doesn't exist
-            }
-            
-            const postData = docSnapshot.data();
-            const docImageSnapshot = await firebaseAdmin.firestore().collection("posts_images").doc(postData.urlId).get();
-            
-            if (!docImageSnapshot.exists) {
-                return null; // Return null if the image document doesn't exist
-            }
-            
-            const url = docImageSnapshot.data().url;
-            const imageUrl = await firebaseAdmin.storage().bucket().file(url).getSignedUrl({ action: 'read', expires: '01-01-2025' });
-            
-            return { id: docSnapshot.id, ...postData, imageUrl };
-        } catch (error) {
-            console.error("Error fetching post:", error);
-            throw error; // Rethrow the error to the caller for proper error handling
-        }
-    }
+  static async addImageToDatabase(url) {
+    const posts_images_id = firebaseAdmin
+      .firestore()
+      .collection("posts_images")
+      .doc().id;
+    const postRef = await firebaseAdmin
+      .firestore()
+      .collection("posts_images")
+      .doc(posts_images_id)
+      .set({
+        url,
+      });
+    return posts_images_id;
+  }
 
-    static async getPostsById(categoryId) {
-        try {
-
-            
-
-            const categorySnapshot = await firebaseAdmin.firestore().collection("categorys")
-            .doc(categoryId)
+  static async getAllPosts() {
+    try {
+      const snapshot = await firebaseAdmin
+        .firestore()
+        .collection("posts")
+        .get();
+      const posts = await Promise.all(
+        snapshot.docs.map(async (doc) => {
+          const postData = doc.data();
+          const docSnapshot = await firebaseAdmin
+            .firestore()
+            .collection("posts_images")
+            .doc(postData.urlId)
             .get();
-
-            const categoryName = categorySnapshot.data().pavadinimas;
-
-            const snapshot = await firebaseAdmin.firestore().collection("posts")
-                .where("categoryId", "==", categoryId) // Replace 'specificCategoryId' with the ID you want to filter by
-                .get();
-        
-            const posts = await Promise.all(snapshot.docs.map(async doc => {
-                const postData = doc.data();
-                const docSnapshot = await firebaseAdmin.firestore().collection("posts_images").doc(postData.urlId).get(); 
-                const url = docSnapshot.data().url; // Assuming 'url' is the field containing the storage file URL
-                const imageUrl = await firebaseAdmin.storage().bucket().file(url).getSignedUrl({ action: 'read', expires: '01-01-2025' });
-                return { id: doc.id, ...postData, imageUrl };
-            }));
-            
-            return { categoryName, posts };
-        } catch (error) {
-           
-        }
+          const url = docSnapshot.data().url; // Assuming 'url' is the field containing the storage file URL
+          const imageUrl = await firebaseAdmin
+            .storage()
+            .bucket()
+            .file(url)
+            .getSignedUrl({ action: "read", expires: "01-01-2025" });
+          return { id: doc.id, ...postData, imageUrl };
+        })
+      );
+      return posts;
+    } catch (error) {
+      console.error("Error fetching all posts:", error);
+      throw error; // Rethrow the error to the caller for proper error handling
     }
+  }
 
-    
-    
-    
-    
+  static async getPostById(postId) {
+    try {
+      const docSnapshot = await firebaseAdmin
+        .firestore()
+        .collection("posts")
+        .doc(postId)
+        .get();
+
+      if (!docSnapshot.exists) {
+        return null; // Return null if the post doesn't exist
+      }
+
+      const postData = docSnapshot.data();
+      const docImageSnapshot = await firebaseAdmin
+        .firestore()
+        .collection("posts_images")
+        .doc(postData.urlId)
+        .get();
+
+      if (!docImageSnapshot.exists) {
+        return null; // Return null if the image document doesn't exist
+      }
+
+      const url = docImageSnapshot.data().url;
+      const imageUrl = await firebaseAdmin
+        .storage()
+        .bucket()
+        .file(url)
+        .getSignedUrl({ action: "read", expires: "01-01-2025" });
+
+      return { id: docSnapshot.id, ...postData, imageUrl };
+    } catch (error) {
+      console.error("Error fetching post:", error);
+      throw error; // Rethrow the error to the caller for proper error handling
+    }
+  }
+
+  static async getPostsById(categoryId) {
+    try {
+      const categorySnapshot = await firebaseAdmin
+        .firestore()
+        .collection("categorys")
+        .doc(categoryId)
+        .get();
+
+      const categoryName = categorySnapshot.data().pavadinimas;
+
+      const snapshot = await firebaseAdmin
+        .firestore()
+        .collection("posts")
+        .where("categoryId", "==", categoryId) // Replace 'specificCategoryId' with the ID you want to filter by
+        .get();
+
+      const posts = await Promise.all(
+        snapshot.docs.map(async (doc) => {
+          const postData = doc.data();
+          const docSnapshot = await firebaseAdmin
+            .firestore()
+            .collection("posts_images")
+            .doc(postData.urlId)
+            .get();
+          const url = docSnapshot.data().url; // Assuming 'url' is the field containing the storage file URL
+          const imageUrl = await firebaseAdmin
+            .storage()
+            .bucket()
+            .file(url)
+            .getSignedUrl({ action: "read", expires: "01-01-2025" });
+          return { id: doc.id, ...postData, imageUrl };
+        })
+      );
+
+      return { categoryName, posts };
+    } catch (error) {}
+  }
 }
 
 module.exports = Post;
