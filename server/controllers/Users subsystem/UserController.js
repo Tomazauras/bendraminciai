6b17179db99a8f710761857036eb55c911fac681
vyteniskri <krisciunas.vytenis@gmail.com>
2024-05-20 03:35:36 +0300

    kategoriju stiliaus upgrade

--- server/controllers/Users subsystem/UserController.js
+++ server/controllers/Users subsystem/UserController.js
@@ -16,7 +16,6 @@ class UserController {
         try {
             const { id } = req.body; 
             const type = await User.getType(id);
-            console.log(type);
             res.send({ Alldata: type});
         } catch (error) {
 
